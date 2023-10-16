d9c48e57f113b480dbef10d43ac47f187ba30863
Francisco Rocamora <147003295+FranRocamora@users.noreply.github.com>
2023-10-15 22:00:20 -0400

    Add files via upload

---
+++ Module 4/src/Exercise2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the side: ");
+        double side = input.nextDouble();
+
+        double area = (6 * side * side) / (4 * Math.tan(Math.PI / 6));
+        
+        System.out.printf("The area of the hexagon is %.2f", area);
+    }
+    
+}
